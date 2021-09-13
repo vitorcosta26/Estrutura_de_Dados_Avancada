e57de2bb5f3b784afbeee065d636ebc198b22e49
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2021-09-13 13:59:00 -0300

    Árvore Genérica

---
+++ Python/Arvore/Arvore Generica/arvore_generica.py
@@ -0,0 +1,67 @@
+
+class Arvore:
+    def __init__(self, elemento):
+        self.elemento = elemento
+        self.filhos = []
+        self.pai = None
+    
+    def set_filho(self, filho):
+        filho.pai = self
+        self.filhos.append(filho)
+    
+    def get_raiz(self):
+        if self.get_nivel() == 0 and self.pai == None:
+            print(self.elemento)
+        else:
+            return self.pai.get_raiz()
+    
+    def get_nos(self):
+        if self.pai == None:
+            pass
+        else:
+            print(self.elemento)
+        if self.filhos:
+            for filho in self.filhos:
+                filho.get_nos()
+    
+    def get_folhas(self):
+        if self.filhos:
+            for filho in self.filhos:
+                filho.get_folhas()
+        else:
+            print(self.elemento)
+    
+    def get_grau(self):
+        return len(self.filhos)
+    
+    def get_altura(self):
+        altura = 0
+        ancestral = self.pai
+        while ancestral:
+            altura += 1
+            ancestral = ancestral.pai
+        return altura
+    
+    def get_profundidade(self):
+        profundidade = 0
+        ancestral = self.pai
+        while ancestral:
+            profundidade += 1
+            ancestral = ancestral.pai
+        return profundidade
+    
+    def get_nivel(self):
+        nivel = 0
+        ancestral = self.pai
+        while ancestral:
+            nivel += 1
+            ancestral = ancestral.pai
+        return nivel
+
+    def get_arvore(self):
+        espacos = " " * self.get_nivel() * 2
+        prefixo = espacos + "|_" if self.pai else ""
+        print(prefixo + str(self.elemento))
+        if self.filhos:
+            for filho in self.filhos:
+                filho.get_arvore()
